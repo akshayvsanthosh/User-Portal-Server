7fc4201e4d6f84d4d1ce505187a16bcbf40f46e5
akshayvsanthosh <akshaysanthosh12345@gmail.com>
2024-07-04 11:04:28 +0530

    user-portal-server completed

---
+++ index.js
@@ -0,0 +1,16 @@
+// import json-server
+const jsonServer = require('json-server')
+// create server for media player app
+const UPServer = jsonServer.create()
+// create middleware used by JSON server
+const middleware = jsonServer.defaults()
+//  set up route for json file in server
+const route = jsonServer.router('db.json')
+// set up port for running server app
+const PORT = 3000 || process.env.PORT
+
+UPServer.use(middleware)
+UPServer.use(route)
+UPServer.listen(PORT,()=>{
+    console.log(`Media Player Server Started at port ${PORT} and waiting for client request`);
+})
